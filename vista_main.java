2087ad5e8c0f53d8fb0dea98e828467fda30a1cc
UsiasSG <160407522+UsiasSG@users.noreply.github.com>
2024-07-30 14:15:08 -0600

    Add files via upload

---
+++ vista_main.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author saqui
+ */
+public class vista_main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        frm_cliente frm = new frm_cliente();
+        frm.show();
+        
+        
+        // TODO code application logic here
+    }
+    
+}
